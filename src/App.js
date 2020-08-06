080a4d932b83c126272fc92172c467b78e0857c9
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-08-06 13:44:35 +0200

    currency setting

--- src/App.js
+++ src/App.js
@@ -4,21 +4,40 @@ import CurrencyRow from "./Components/CurrencyRow";
 
 function App() {
   const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState();
+  const [exchangeRate, setExchangeRate] = useState();
   const BASE_URL = "https://api.exchangeratesapi.io/latest";
   console.log(currencyOptions);
+  console.log(toCurrency);
+  console.log(fromCurrency);
+
   // pass the base currency & other currencys to option in CurrencyRow component
   useEffect(() => {
     fetch(BASE_URL)
       .then((resp) => resp.json())
       .then((data) => {
+        const firstCurrency = Object.keys(data.rates)[0];
         setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+        setFromCurrency(data.base);
+        setToCurrency(firstCurrency);
+
+        setExchangeRate(data.rates[firstCurrency]);
       });
   }, []);
   return (
     <div className="App">
       <h1>Exchange calculator</h1>
-      <CurrencyRow currencyOptions={currencyOptions} />
-      <CurrencyRow currencyOptions={currencyOptions} />
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+        changeCurrency={(e) => setFromCurrency(e.target.value)}
+      />
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+        changeCurrency={(e) => setToCurrency(e.target.value)}
+      />
     </div>
   );
 }
--- src/Components/CurrencyRow.js
+++ src/Components/CurrencyRow.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
-export default function CurrencyRow({ currencyOptions }) {
+export default function CurrencyRow({
+  currencyOptions,
+  selectedCurrency,
+  changeCurrency,
+}) {
   return (
     <div>
       <input type="number" className="currency__input" />
-      <select>
+      <select value={selectedCurrency} onChange={changeCurrency}>
         {/* populate options with currencyOptions from props */}
         {currencyOptions.map((option) => (
           <option key={option} value={option}>
